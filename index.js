0484dd6629d3a94f991796797580db83cf904b01
Erik Russell <erikrussells@gmail.com>
2022-10-03 09:04:30 -0400

    commit tree 8df4b00eb4f53e728bc921b9aae6586fcc2346d8

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Write your solution in this file!
+// let employee = {
+//     name: "Erik",
+//     street: "11 Broadway"
+// }
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value
+//     return employee
+// }
+    
+// function deleteFromEmployeeByKey (employee, key) {
+//         let newEmployee = {...employee}
+//         delete newEmployee[key]
+//         return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee,key) {
+//         delete employee[key]
+//         return employee
+// }
+
+
+
+
+
+// let employee = {
+//     name: "Erik",
+//     streetAddress: "271 Green st."
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return {
+//         ...employee, ...{
+//             [key]: value
+//         }
+        
+//     }
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//         employee[key] = value;
+//         return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     let newEmployee = { ...employee.key }
+
+//     delete newEmployee[key]
+
+//     return newEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key]
+//     return employee
+// }
+
+
+
+
+
+let employee = {
+    name: "Erik",
+    streetAddress: "217 Green st"
+}
+
+let newEmployee 
